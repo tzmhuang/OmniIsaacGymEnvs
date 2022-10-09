e2994f9b9555f81cf14a35eb2983c35732746e13
tzm <tzm@huang.com>
2022-10-09 13:26:53 -0400

    Added BlindHumanoid task

--- omniisaacgymenvs/utils/task_util.py
+++ omniisaacgymenvs/utils/task_util.py
@@ -44,6 +44,7 @@ def initialize_task(config, env, init_sim=True):
     from omniisaacgymenvs.tasks.quadcopter import QuadcopterTask
     from omniisaacgymenvs.tasks.shadow_hand import ShadowHandTask
     from omniisaacgymenvs.tasks.crazyflie import CrazyflieTask
+    from omniisaacgymenvs.tasks.blind_humanoid_room import BlindHumanoidLocomotionTask
     
     # Mappings from strings to environments
     task_map = {
@@ -62,6 +63,7 @@ def initialize_task(config, env, init_sim=True):
         "ShadowHand": ShadowHandTask,
         "ShadowHandOpenAI_FF": ShadowHandTask,
         "ShadowHandOpenAI_LSTM": ShadowHandTask,
+        "BlindHumanoid": BlindHumanoidLocomotionTask,
     }
 
     cfg = sim_config.config
